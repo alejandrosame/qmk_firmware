e19425c46447abc37aa01339996e8cfe53dfb232
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-10-20 22:47:04 +0200

    [WIP] Pin nixpkgs to avoid Python3.10 hatchling issue
    ```
    nix flake check
    warning: Git tree '/home/alesame/Projects/me/qmk_firmware' is dirty
    error: flake attribute 'devShells.x86_64-darwin.x86_64-darwin' is not a derivation
    (use '--show-trace' to show detailed location information)
    ```

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "QMK firmware";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/c0e881852006b132236cbf0301bd1939bb50867e";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
@@ -8,12 +10,19 @@
     nixpkgs,
     flake-utils,
   }:
-    flake-utils.lib.eachDefaultSystem
+  # flake-utils.lib.eachDefaultSystem # TODO: Make explicit filtering
+    flake-utils.lib.eachSystem [
+      "aarch64-linux"
+      #"aarch64-darwin"
+      #"i686-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ]
     (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShell = pkgs.callPackage ./shell.nix {inherit pkgs;};
+        devShells.${system}.default = pkgs.callPackage ./shell.nix {inherit pkgs;};
       }
     );
 }
