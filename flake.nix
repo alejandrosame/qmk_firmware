00f5e3963a939cc28e43508ede708915aca1d2db
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-10-22 12:29:23 +0200

    [WIP] Fix import error
    ```
    nix develop
    warning: Git tree '/home/alesame/Projects/me/qmk_firmware' is dirty
    error: infinite recursion encountered
    
           at /nix/store/54xsxxsx2vmlyfw4q2ads0jv3iayv1lc-source/pkgs/stdenv/generic/make-derivation.nix:226:11:
    
              225|           depsHostHost                = lib.elemAt (lib.elemAt dependencies 1) 0;
              226|           buildInputs                 = lib.elemAt (lib.elemAt dependencies 1) 1;
                 |           ^
              227|           depsTargetTarget            = lib.elemAt (lib.elemAt dependencies 2) 0;
    (use '--show-trace' to show detailed location information)
    ```

--- flake.nix
+++ flake.nix
@@ -9,20 +9,10 @@
     self,
     nixpkgs,
     flake-utils,
-  }:
-  # flake-utils.lib.eachDefaultSystem # TODO: Make explicit filtering
-    flake-utils.lib.eachSystem [
-      "aarch64-linux"
-      #"aarch64-darwin"
-      #"i686-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ]
-    (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.${system}.default = pkgs.callPackage ./shell.nix {inherit pkgs;};
-      }
-    );
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.callPackage ./shell.nix {inherit pkgs;};
+  };
 }
