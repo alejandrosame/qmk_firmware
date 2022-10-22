ae22ba5f9f56675b26942aaa29e5e18a960640df
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-10-22 13:44:38 +0200

    Fix infinite recursion error

--- flake.nix
+++ flake.nix
@@ -9,10 +9,23 @@
     self,
     nixpkgs,
     flake-utils,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.callPackage ./shell.nix {inherit pkgs;};
-  };
+  }:
+    {
+      # We specify sources via Niv: use "niv update nixpkgs" to update nixpkgs, for example.
+      sources = import ./util/nix/sources.nix {};
+
+      pythonOverlay = import ./util/nix/python-overlay.nix;
+    }
+    // flake-utils.lib.eachSystem ["x86_64-linux"] (
+      system: let
+        pkgs = import nixpkgs {
+          overlays = [self.pythonOverlay];
+          inherit system;
+        };
+
+        poetry2nix = pkgs.callPackage (import self.sources.poetry2nix) {};
+      in {
+        devShells.default = pkgs.callPackage ./shell.nix {inherit pkgs poetry2nix;};
+      }
+    );
 }
