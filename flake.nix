332cb68af8a8df123960eca7b0900aadde136a91
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-10-28 13:11:24 +0200

    Document inspiration for pattern used

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "QMK firmware";
 
   # Input dependencies are loaded on the dirty flake side via Niv.
+  # This pattern is inspired by https://github.com/crazazy/niv-flakes.
   outputs = {self}: let
     dirtyFlake = import ./util/nix/dirty-flake.nix;
     dirtyFlakeInputs = dirtyFlake.inputs // {inherit self;};
